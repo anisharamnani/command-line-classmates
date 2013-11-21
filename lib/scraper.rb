a1863d1960ea0c773761ec7719b04a917494e35e
anisharamnani <anisha.ramnani@gmail.com>
2013-11-21 16:13:35 -0500

    nokogiri search

---
+++ lib/scraper.rb
@@ -0,0 +1,36 @@
+# Ashley's gist--command line classmates
+# https://gist.github.com/ashleygwilliams/7586141
+# require the libraries that we need 
+
+require 'open-uri'
+require 'nokogiri'
+
+class Scraper
+	attr_reader :html
+
+	def initialize(url)
+		download = open(url) 
+		# into the class Nokogiri 
+		# using the method HTML 
+		# passing the paramater download 
+		# we want to use HTML function to make it into ruby
+		# download: originally a temp file 
+		@html = Nokogiri::HTML(download)
+	end
+
+	def get_students_names
+		html.search("div.face.front h3").text
+	end
+
+	def get_student_blogs
+		html.css(".blog").map {|link| link['href']}
+	end 
+
+	def get_twitter_urls
+		html.css(".twitter").text.split
+	end 
+end 
+
+my_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
+
+puts my_scraper.get_student_blogs
